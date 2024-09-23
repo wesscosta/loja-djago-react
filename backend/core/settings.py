a704c9be2c00fbe5f1bf3ac60ca423e211c01529
Weslley Costa <weslleycosta.ti@gmail.com>
2024-09-23 13:39:07 -0300

    CI: correção MEDIA_ROOT e MEDIA_URL no  backend/settings.py e criação das URLS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -141,8 +141,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 STATICFILES_DIRS = [
     BASE_DIR/'static',

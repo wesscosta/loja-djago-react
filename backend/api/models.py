c9b2c5400ef93c4b3320bbc3bf2b2161910ad7d0
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-01 12:08:04 -0300

    feat: criação do model OrderItem

--- backend/api/models.py
+++ backend/api/models.py
@@ -49,3 +49,15 @@ class Order(models.Model):
     
     def __str__(self) -> str:
         return str(self.createdAt)
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null= True)
+    name = models.CharField(max_length=200,null=True, blank=True)
+    qty =models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=12,decimal_places=2, null=True,blank=True)
+    image = models.CharField(max_length=200,null=True,blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self) -> str:
+        return str(self.name)

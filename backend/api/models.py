e8885c33dc2553a8aa7b993bd1488065f38cc28e
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-01 11:54:11 -0300

    feat: criação do model Review

--- backend/api/models.py
+++ backend/api/models.py
@@ -21,3 +21,15 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name + " | " + self.brand+ " | " + str(self.price)
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    nome = models.CharField(max_length=200, null=True,blank=True)
+    rating = models.IntegerField(null=True,blank=True,default=0)
+    comment = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+    
+    def __str__(self) -> str:
+        return str(self.rating)

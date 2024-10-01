fc664b65f8166821132875ff078b4c9c95d83dd1
Weslley Costa <weslleycosta.ti@gmail.com>
2024-10-01 12:46:23 -0300

    feat: criação das urls

---
+++ backend/api/urls/orders_urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from api.views import order_views as views
+
+urlpatterns = [
+    path('add/', views.addOrderItems, name = 'orders-add'),
+    path('myyorders/', views.getMyOrders, name='myorders'),
+    path('<str:pk>/', views.getOrderById, name = 'user-order'),
+    path('<str:pk>/pay', views.updateOrderToPaid, name='pay'),
+]
